f150bb8d73aab140530c5b5848580f56ced39cb6
cmduquer <cmduquer@gmail.com>
2019-07-30 13:19:28 -0500

    Update OwnerRepository.java

--- src/main/java/com/ingweb/car/repository/OwnerRepository.java
+++ src/main/java/com/ingweb/car/repository/OwnerRepository.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.ingweb.car.model.Owner;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:4200")
 public interface OwnerRepository extends JpaRepository<Owner, Long> {
 	
 	List<Owner> findByDni(String dni);
